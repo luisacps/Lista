1bed1f76838f3fe0c11dd842b0dd9267fb0d209d
20221tiimi0068 <luisacpsantos@gmail.com>
2024-05-14 10:57:34 -0300

    atualizacao 14/05

--- app/src/main/java/santos/pimentel/lista_/adapter/MyAdapter.java
+++ app/src/main/java/santos/pimentel/lista_/adapter/MyAdapter.java
@@ -33,7 +33,7 @@ public class MyAdapter extends RecyclerView.Adapter {
         LayoutInflater inflater = LayoutInflater.from(mainActivity);
         // uso do layoutinflater para criar os elementos referentes a um item
         View v = inflater.inflate(R.layout.item_list, parent, false);
-        // guarda a view num objeto viewholder e retorna ele
+        // guarda a view num objeto viewholder e retorna el
         return new MyViewHolder(v);
     }
 
